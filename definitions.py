357345a017b211a631fcb143305bb750585d712f
zai1208 <63334410+zai1208@users.noreply.github.com>
2022-08-06 21:11:47 +1000

    .

---
+++ definitions.py
@@ -0,0 +1,33 @@
+import pygame
+
+nodes = {
+"button" : {
+"image" : "",
+
+},
+"toggle" : "",
+"switch" : "",
+"object" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+}
+
+properties = {
+"selectable" : "",
+"controllable" : "(true, false)",
+"rect" : "(x, y, width, height)",
+"pickable" : "(true, false)",
+"current" : "(true, false)",
+"selected" : "(true, false)",
+}
